fc85ce45843ee1ca557223e04aa8b33240306a0f
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2023-09-13 04:29:22 -0700

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  const styles = {};
+  return (
+    <header className="header">
+      <h1 style={styles}>Pizza House</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </main>
+  );
+}
+
+const hour = new Date().getHours();
+const openHoue = 12;
+const closeHour = 22;
+const isOpen = hour >= openHoue && hour <= closeHour;
+
+function Footer() {
+  return <div className="footer">we are now {isOpen ? "Open" : "Closed"}</div>;
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas\spinaci.jpg" alt="Pizza spinaci" />
+      <h3>Pizza Spinaci</h3>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
