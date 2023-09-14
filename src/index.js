6f511cb68cbb94979f35ca225692b6f6804c354c
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2023-09-14 02:21:56 -0700

    Props and States | Intro

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function Header() {
   const styles = {};
   return (
     <header className="header">
-      <h1 style={styles}>Pizza House</h1>
+      <h1 style={styles}>Pizza House </h1>
     </header>
   );
 }
@@ -25,12 +25,33 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas\spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Toamato, mushrooms"
+        price={34}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 
@@ -41,16 +62,6 @@ const isOpen = hour >= openHoue && hour <= closeHour;
 
 function Footer() {
   return <div className="footer">we are now {isOpen ? "Open" : "Closed"}</div>;
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas\spinaci.jpg" alt="Pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
